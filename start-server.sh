2df9495ee5d8cc1d2256a012422d361a81b28c16
The Random Gamer <24951208+TRGReal@users.noreply.github.com>
2019-11-17 02:14:51 +0800

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -4,13 +4,13 @@
 echo "Running $BASH as shell"
 echo "$PATH"
 echo "If you don't find java in these paths, you may have to add it."
-echo "Starting server, please wait..."
-echo "GUI or nah [y/N]: " && read -n 1 guiOrNot
-if [[ guiOrNot == y ]];
+echo "Starting Bungee, please wait..."
+echo "Would you like a GUI? [Y/n]: " && read -n 1 guiOrNot
+if [[ guiOrNot == n ]];
 then {
-	java -Xmx1024M -Xms1024M -jar server.jar
+	java -Xmx1024M -Xms1024M -jar server.jar nogui
 }
 else
-	java -Xmx1024M -Xms1024M -jar server.jar nogui
+	java -Xmx1024M -Xms1024M -jar server.jar
 fi
-echo "Server Closed."
+echo "Server Crash? The script has ended."
